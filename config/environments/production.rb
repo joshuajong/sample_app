2ec758ee562e2011f41fc1023e1c01e0230438db
Joshua Jong <joshuajongsh@gmail.com>
2021-07-18 06:39:54 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,10 +110,10 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   
-  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://obscure-ridge-61515.herokuapp.com' 
-  config.action_mailer.default_url_options = { host: host } 
+  host = 'https://obscure-ridge-61515.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
